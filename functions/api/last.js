0418695ef9a9980cb57c7cca51b82cd1a36e38b8
Keith Casko <kcasko@wgu.edu>
2025-04-05 00:21:43 -0400

    removed test files

--- functions/api/last.js
+++
@@ -1,9 +0,0 @@
-// functions/api/test.js
-export async function onRequestGet() {
-  const data = { status: "ok", message: "Test function is working!" };
-  // Ensure headers are set correctly for JSON
-  const headers = {
-    'Content-Type': 'application/json'
-  };
-  return new Response(JSON.stringify(data), { status: 200, headers: headers });
-}
--- functions/api/test.js
+++
@@ -1,7 +0,0 @@
-// functions/api/test.js
-export async function onRequestGet() {
-  const data = { message: "Hello from test function!" };
-  return new Response(JSON.stringify(data), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
